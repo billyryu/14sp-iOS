bb5d4d0aa8c7e1496fab647a4dc1d408697c6967
Billy Ryu <billyryu@billyryu.com>
2014-04-08 11:10:16 -0400

    Midterm Project Game

---
+++ Midterm-Pond/soundpond/GameOverScene.h
@@ -0,0 +1,13 @@
+//
+//  GameOverScene.h
+//  soundpond
+//
+//  Created by Billy Ryu on 4/5/14.
+//  Copyright (c) 2014 Billy Ryu. All rights reserved.
+//
+
+#import "MyScene.h"
+
+@interface GameOverScene : MyScene
+
+@end
---
+++ Midterm-Pond/soundpond/WinScene.h
@@ -0,0 +1,13 @@
+//
+//  WinScene.h
+//  soundpond
+//
+//  Created by Billy Ryu on 4/8/14.
+//  Copyright (c) 2014 Billy Ryu. All rights reserved.
+//
+
+#import "MyScene.h"
+
+@interface WinScene : MyScene
+
+@end
